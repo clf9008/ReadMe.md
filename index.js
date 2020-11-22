98fc9b7823297667863a2bb2890fe595272a601d
Follett <clf9008@gmail.com>
2020-11-22 15:10:57 -0700

    added dependencies for global memory

---
+++ index.js
@@ -0,0 +1,7 @@
+//Declaring dependencies into global memory 
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+
