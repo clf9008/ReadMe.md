e5c5973b2e0b93d7705d749420c979d7a23461af
Follett <clf9008@gmail.com>
2020-11-22 15:20:47 -0700

    added prompt questions to generate the Readme.MD

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const inquirer = require("inquirer");
 const generateReadme = require("./utils/generateReadme")
 const writeFileAsync = util.promisify(fs.writeFile);
 
+//Prompts that will be displayed to the user in command line to generate ReadMe
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What would you like to name this project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a breif description of the project: ",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation prcess for the projct: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What will be the usage of this application?",
+        }
+
+    ])
+}
